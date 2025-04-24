e1c78eeef2e5a28f1cda0936aaf76a697780b4f7
juseoki <61035382+juseoki@users.noreply.github.com>
2025-04-24 14:16:34 +0900

    Update main.jsx
    src/main.jsx 수정

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import { HashRouter } from 'react-router-dom';
 
-<HashRouter>
-  <App />
-</HashRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
